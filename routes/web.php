51c15cbc5eab9ca90517295f2deaf4f317afb629
Josh <lishufang1121@gmail.com>
2022-04-30 00:49:17 +0800

    Complete of Fallback route in Laravel

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+// This will only match text that does not begin with the API slash.
+Route::get('/{any?}', function () {
     return view('welcome');
-});
+})->where('any', '^(?!api\/)[\/\w\.\,-]*');
