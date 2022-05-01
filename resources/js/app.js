58ed3d92f53c928442a9b828c2fa441314fd2cf9
Josh <lishufang1121@gmail.com>
2022-05-01 17:53:19 +0800

    Complete of Section 2:Laravel API & Vue routing and basics

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,11 +4,15 @@
  * building robust, powerful web applications using Vue and Laravel.
  */
 
-require('./bootstrap');
-import router from "./routes";
-import VueRouter from "vue-router";
 
-window.Vue = require('vue').default;
+
+require("./bootstrap");
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Index from "./Index";
+import router from "./routes";
+
+window.Vue = require("vue");
 
 /**
  * The following block of code may be used to automatically register your
@@ -21,8 +25,8 @@ window.Vue = require('vue').default;
 // const files = require.context('./', true, /\.vue$/i)
 // files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
 
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-
+// Vue.component('example-component', require('./components/ExampleComponent.vue').default);
+// Vue.component('example-2', require('./components/Example2.vue').default);
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
@@ -32,8 +36,11 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 Vue.use(VueRouter);
 
 const app = new Vue({
-    el: '#app',
+    el: "#app",
     router,
+    components: {
+        index: Index,
+    },
 });
 
 
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,19 +1,37 @@
-import ExampleComponent from "./components/ExampleComponent";
 import VueRouter from "vue-router";
+import Example2 from "./components/Example2";
+import ExampleComponent from "./components/ExampleComponent";
 
 const routes =
     [
         {
-            path: '/',
+            path: "/",
             component: ExampleComponent,
-            name: 'home',
+            name: "home",
         },
 
+        {
+            path: "/second",
+            component: Example2,
+            name: "second",
+        },
 
     ];
 
+// When using createWebHistory(),
+//     the URL will look "normal," e.g. https://example.com/user/id. Beautiful!
+// Here comes a problem, though:
+// Since our app is a single page client side app,
+//     without a proper server configuration,
+//     the users will get a 404 error if they access https://example.com/user/id directly in their browser. Now that's ugly.
+//
+//     Not to worry: To fix the issue,
+//     all you need to do is add a simple catch-all fallback route to your server.
+//     If the URL doesnt match any static assets,
+//     it should serve the same index.html page that your app lives in. Beautiful, again!
 const router = new VueRouter({
-    routes, // short for 'routes: routes'
+    routes, // short for `routes: routes`
+    mode: "history",
 });
 
 export default router;
